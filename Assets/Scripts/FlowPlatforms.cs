b9f276286bea63c4ba4cf6670f32ab0fc924b842
Jordan Powers <jordanpowers1227@gmail.com>
2020-12-12 11:41:49 -0800

    Remove hack, separate flow code to new file

---
+++ Assets/Scripts/FlowPlatforms.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class FlowPlatforms : MonoBehaviour
+{
+    private enum Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+
+    private List<Move> moves = new List<Move>();
+    private GameState state;
+
+
+    private class Move
+    {
+        public Vector2 from;
+        public Vector2 to;
+        public Transform who;
+
+        private Vector2 pos;
+        private Vector2 vel = Vector2.zero;
+
+        private float maxSpd = 0.1f;
+        private float acc = 0.01f;
+
+        bool done = false;
+        public Move(Vector2 from, Vector2 to, Transform who)
+        {
+            this.from = from;
+            this.to = to;
+            this.who = who;
+
+            pos = from;
+        }
+
+        public void tick()
+        {
+            vel += ((to - from).normalized * acc);
+            if (vel.magnitude > maxSpd)
+            {
+                vel = vel.normalized * maxSpd;
+            }
+            pos += vel;
+
+            done = done || (vel.x > 0 && pos.x > to.x);
+            done = done || (vel.x < 0 && pos.x < to.x);
+            done = done || (vel.y > 0 && pos.y > to.y);
+            done = done || (vel.y < 0 && pos.y < to.y);
+            if (done)
+            {
+                pos = to;
+            }
+
+            who.position = pos;
+        }
+
+        public bool isDone()
+        {
+            return done;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = Object.FindObjectOfType<GameState>();
+        Assert.IsNotNull(state);
+    }
+
+    private void checkForMove(StateTrackedObject[,] staticState, StateTrackedObject[,] dynamicState, StateTrackedObject[,] nextState, int x, int y, int dx, int dy)
+    {
+        StateTrackedObject staticCurr = staticState[x, y];
+        StateTrackedObject dynamicCurr = dynamicState[x, y];
+        if (staticCurr == null)
+        {
+            return;
+        }
+        else if (dynamicCurr == null)
+        {
+            nextState[x, y] = staticCurr;
+            return;
+        }
+        else if (dynamicCurr is MovPlatformBehavior)
+        {
+            int nextX = x;
+            int nextY = y;
+            while (true)
+            {
+                int testX = nextX + dx;
+                int testY = nextY + dy;
+
+                if (testX < 0 || testY < 0
+                    || testX >= staticState.GetLength(0) || testY >= staticState.GetLength(1)
+                    || nextState[testX, testY] == null
+                    || !(nextState[testX, testY] is WaterBehavior))
+                {
+                    nextState[x, y] = staticCurr;
+                    nextState[nextX, nextY] = dynamicCurr;
+                    if (x != nextX || y != nextY)
+                    {
+                        moves.Add(new Move(this.state.gridToGlobal(new Vector2(x, y)), this.state.gridToGlobal(new Vector2(nextX, nextY)), dynamicCurr.transform));
+                    }
+                    return;
+                }
+
+                nextX = testX;
+                nextY = testY;
+            }
+        }
+        else
+        {
+            Assert.IsTrue(false);
+        }
+
+    }
+
+    private void flow(Direction direction)
+    {
+        StateTrackedObject[,] staticState = this.state.buildStaticState();
+        StateTrackedObject[,] dynamicState = this.state.buildDynamicState();
+        int width = staticState.GetLength(0);
+        int height = staticState.GetLength(1);
+        StateTrackedObject[,] nextState = new StateTrackedObject[width, height];
+
+        switch (direction)
+        {
+            case Direction.DOWN:
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        checkForMove(staticState, dynamicState, nextState, x, y, 0, -1);
+                    }
+                }
+                break;
+            case Direction.UP:
+                for (int y = height - 1; y >= 0; y--)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        checkForMove(staticState, dynamicState, nextState, x, y, 0, 1);
+                    }
+                }
+                break;
+            case Direction.LEFT:
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        checkForMove(staticState, dynamicState, nextState, x, y, -1, 0);
+                    }
+                }
+                break;
+            case Direction.RIGHT:
+                for (int x = width - 1; x >= 0; x--)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        checkForMove(staticState, dynamicState, nextState, x, y, 1, 0);
+                    }
+                }
+                break;
+            default:
+                Assert.IsTrue(false);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moves.Count == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                flow(Direction.UP);
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                flow(Direction.DOWN);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                flow(Direction.LEFT);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                flow(Direction.RIGHT);
+            }
+        }
+
+
+        List<Move> doneMoves = new List<Move>();
+        foreach (Move move in moves)
+        {
+            move.tick();
+            if (move.isDone())
+            {
+                doneMoves.Add(move);
+            }
+        }
+        foreach (Move move in doneMoves)
+        {
+            moves.Remove(move);
+        }
+    }
+
+    
+}
--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -6,14 +6,6 @@ using UnityEngine.Assertions;
 
 public class GameState : MonoBehaviour
 {
-
-    private enum Direction
-    {
-        UP,
-        DOWN,
-        LEFT,
-        RIGHT
-    }
     
     private Vector2 minpos = new Vector2(0, 0);
     private Vector2 maxpos = new Vector2(0, 0);
@@ -21,57 +13,6 @@ public class GameState : MonoBehaviour
     private List<StateTrackedObject> stillPlatforms = new List<StateTrackedObject>();
     private List<StateTrackedObject> movPlatforms = new List<StateTrackedObject>();
 
-    private List<Move> moves = new List<Move>();
-
-
-    private class Move
-    {
-        public Vector2 from;
-        public Vector2 to;
-        public Transform who;
-
-        private Vector2 pos;
-        private Vector2 vel = Vector2.zero;
-
-        private float maxSpd = 0.1f;
-        private float acc = 0.01f;
-
-        bool done = false;
-        public Move(Vector2 from, Vector2 to, Transform who)
-        {
-            this.from = from;
-            this.to = to;
-            this.who = who;
-
-            pos = from;
-        }
-
-        public void tick()
-        {
-            vel += ((to - from).normalized * acc);
-            if(vel.magnitude > maxSpd)
-            {
-                vel = vel.normalized * maxSpd;
-            }
-            pos += vel;
-            
-            done = done || (vel.x > 0 && pos.x > to.x);
-            done = done || (vel.x < 0 && pos.x < to.x);
-            done = done || (vel.y > 0 && pos.y > to.y);
-            done = done || (vel.y < 0 && pos.y < to.y);
-            if(done)
-            {
-                pos = to;
-            }
-
-            who.position = pos;
-        }
-
-        public bool isDone()
-        {
-            return done;
-        }
-    }
 
     private void updateBounds(Transform trans) {
         minpos.x = Math.Min(trans.position.x, minpos.x);
@@ -107,8 +48,10 @@ public class GameState : MonoBehaviour
 
     
 
-    private StateTrackedObject[,] buildState()
+    public StateTrackedObject[,] buildState()
     {
+        Assert.IsTrue(minpos.x < maxpos.x);
+        Assert.IsTrue(minpos.y < maxpos.y);
         StateTrackedObject[,] state = new StateTrackedObject[((int)maxpos.x - (int)minpos.x) + 1, ((int)maxpos.y - (int)minpos.y) + 1];
         foreach(StateTrackedObject item in water)
         {
@@ -132,140 +75,57 @@ public class GameState : MonoBehaviour
         return state;
     }
 
-    private void checkForMove(StateTrackedObject[,] state, StateTrackedObject[,] nextState, int x, int y, int dx, int dy)
-    {
-        StateTrackedObject curr = state[x, y];
-        if(curr == null)
-        {
-            return;
-        } else if(curr is WaterBehavior || curr is StillPlatformBehavior)
-        {
-            nextState[x, y] = curr;
-            return;
-        } else if(curr is MovPlatformBehavior)
-        {
-            int nextX = x;
-            int nextY = y;
-            while(true)
-            {
-                int testX = nextX + dx;
-                int testY = nextY + dy;
-
-                if(testX < 0 || testY < 0
-                    || testX >= state.GetLength(0) || testY >= state.GetLength(1)
-                    || nextState[testX, testY] == null
-                    || !( nextState[testX, testY] is WaterBehavior) )
-                {
-                    // Yeah, this is a hack
-                    // I need to mark that the space previously occupied by the movPlatform is now free
-                    // However, I don't have easy access to the StateTrackedObject representing the water tile at that location
-                    // As a workaround, I'm using the StateTrackedObject representing the water tile at the space now occupied by the movPlatform
-                    nextState[x, y] = state[nextX, nextY];
-                    nextState[nextX, nextY] = curr;
-                    if(x != nextX || y != nextY)
-                    {
-                        moves.Add(new Move(new Vector2(x, y) + minpos, new Vector2(nextX, nextY) + minpos, curr.transform));
-                    }
-                    return;
-                }
-
-                nextX = testX;
-                nextY = testY;
-            }
-        } else
-        {
-            Assert.IsTrue(false);
-        }
-
-    }
-
-    private void flow(Direction direction)
+    public StateTrackedObject[,] buildStaticState()
     {
         Assert.IsTrue(minpos.x < maxpos.x);
         Assert.IsTrue(minpos.y < maxpos.y);
-        StateTrackedObject[,] state = buildState();
-        StateTrackedObject[,] nextState = new StateTrackedObject[state.GetLength(0), state.GetLength(1)];
-        
-        switch(direction)
+        StateTrackedObject[,] state = new StateTrackedObject[((int)maxpos.x - (int)minpos.x) + 1, ((int)maxpos.y - (int)minpos.y) + 1];
+        foreach (StateTrackedObject item in water)
         {
-            case Direction.DOWN:
-                for(int y = 0; y < state.GetLength(1); y++)
-                {
-                    for(int x = 0; x < state.GetLength(0); x++)
-                    {
-                        checkForMove(state, nextState, x, y, 0, -1);
-                    }
-                }
-                break;
-            case Direction.UP:
-                for(int y = state.GetLength(1) - 1; y >= 0; y--)
-                {
-                    for(int x = 0; x < state.GetLength(0); x++)
-                    {
-                        checkForMove(state, nextState, x, y, 0, 1);
-                    }
-                }
-                break;
-            case Direction.LEFT:
-                for(int x = 0; x < state.GetLength(0); x++)
-                {
-                    for(int y = 0; y < state.GetLength(1); y++)
-                    {
-                        checkForMove(state, nextState, x, y, -1, 0);
-                    }
-                }
-                break;
-            case Direction.RIGHT:
-                for(int x = state.GetLength(0) - 1; x >= 0; x--)
-                {
-                    for(int y = 0; y < state.GetLength(1); y++)
-                    {
-                        checkForMove(state, nextState, x, y, 1, 0);
-                    }
-                }
-                break;
-            default:
-                Assert.IsTrue(false);
-                break;
+            StateTrackedObject curr = state[(int)(item.transform.position.x - minpos.x), (int)(item.transform.position.y - minpos.y)];
+            Assert.IsNull(curr);
+            state[(int)(item.transform.position.x - minpos.x), (int)(item.transform.position.y - minpos.y)] = item;
         }
+        foreach (StateTrackedObject item in stillPlatforms)
+        {
+            StateTrackedObject curr = state[(int)(item.transform.position.x - minpos.x), (int)(item.transform.position.y - minpos.y)];
+            Assert.IsNull(curr);
+            state[(int)(item.transform.position.x - minpos.x), (int)(item.transform.position.y - minpos.y)] = item;
+        }
+
+        return state;
+    }
+
+    public StateTrackedObject[,] buildDynamicState()
+    {
+        Assert.IsTrue(minpos.x < maxpos.x);
+        Assert.IsTrue(minpos.y < maxpos.y);
+        StateTrackedObject[,] state = new StateTrackedObject[((int)maxpos.x - (int)minpos.x) + 1, ((int)maxpos.y - (int)minpos.y) + 1];
+        foreach (StateTrackedObject item in movPlatforms)
+        {
+            StateTrackedObject curr = state[(int)(item.transform.position.x - minpos.x), (int)(item.transform.position.y - minpos.y)];
+            Assert.IsTrue(curr == null || curr is WaterBehavior);
+            state[(int)(item.transform.position.x - minpos.x), (int)(item.transform.position.y - minpos.y)] = item;
+        }
+
+        return state;
+    }
+
+    public Vector2 gridToGlobal(Vector2 grid)
+    {
+        return grid + minpos;
+    }
+
+    public Vector2 globalToGrid(Vector2 global)
+    {
+        Vector2 snapped = new Vector2(Mathf.Floor(global.x), Mathf.Floor(global.y));
+        return snapped - minpos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(moves.Count == 0)
-        {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                flow(Direction.UP);
-            }
-            else if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                flow(Direction.DOWN);
-            }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                flow(Direction.LEFT);
-            }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                flow(Direction.RIGHT);
-            }
-        }
-        
 
-        List<Move> doneMoves = new List<Move>();
-        foreach(Move move in moves)
-        {
-            move.tick();
-            if(move.isDone())
-            {
-                doneMoves.Add(move);
-            }
-        }
-        foreach(Move move in doneMoves)
-        {
-            moves.Remove(move);
-        }
     }
+
 }
